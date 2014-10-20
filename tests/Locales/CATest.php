fa13d611a788029700f5f494deaf637b9977b989
Tony Li <tonyli.lives@gmail.com>
2014-10-20 11:26:01 -0700

    added phpunit testing

---
+++ tests/Locales/CATest.php
@@ -0,0 +1,121 @@
+<?php namespace Localize\Tests\Locales;
+
+use Localize\Localize;
+
+class CATest extends \PHPUnit_Framework_TestCase
+{
+    private $localize;
+
+    public function setup()
+    {
+        $this->localize = new Localize('CA');
+    }
+
+    public function testIsCA()
+    {
+        $this->assertEquals('CA', $this->localize->getLocale());
+    }
+
+    public function testCountry()
+    {
+        $this->assertEquals('CA', $this->localize->country('Canada', true));
+        $this->assertEquals('Canada', $this->localize->country('CANADA'));
+        $this->assertNull($this->localize->country('null'));
+    }
+
+    public function testRegionsShort()
+    {
+        $this->assertNull($this->localize->region('', true));
+
+        $this->assertEquals('AB', $this->localize->region('alberta', true));
+        $this->assertEquals('BC', $this->localize->region('british columbia', true));
+        $this->assertEquals('MB', $this->localize->region('manitoba', true));
+        $this->assertEquals('NB', $this->localize->region('new brunswick', true));
+        $this->assertEquals('NL', $this->localize->region('newfoundland and labrador', true));
+        $this->assertEquals('NT', $this->localize->region('northwest territories', true));
+        $this->assertEquals('NS', $this->localize->region('nova scotia', true));
+        $this->assertEquals('NU', $this->localize->region('nunavut', true));
+        $this->assertEquals('ON', $this->localize->region('ontario', true));
+        $this->assertEquals('PE', $this->localize->region('prince edward island', true));
+        $this->assertEquals('QC', $this->localize->region('quebec', true));
+        $this->assertEquals('SK', $this->localize->region('saskatchewan', true));
+        $this->assertEquals('YT', $this->localize->region('yukon', true));
+
+        $this->assertEquals('AB', $this->localize->region('ab', true));
+        $this->assertEquals('BC', $this->localize->region('bc', true));
+        $this->assertEquals('MB', $this->localize->region('mb', true));
+        $this->assertEquals('NB', $this->localize->region('nb', true));
+        $this->assertEquals('NL', $this->localize->region('nl', true));
+        $this->assertEquals('NT', $this->localize->region('nt', true));
+        $this->assertEquals('NS', $this->localize->region('ns', true));
+        $this->assertEquals('NU', $this->localize->region('nu', true));
+        $this->assertEquals('ON', $this->localize->region('on', true));
+        $this->assertEquals('PE', $this->localize->region('pe', true));
+        $this->assertEquals('QC', $this->localize->region('qc', true));
+        $this->assertEquals('SK', $this->localize->region('sk', true));
+        $this->assertEquals('YT', $this->localize->region('yt', true));
+    }
+
+    public function testRegionsLong()
+    {
+        $this->assertNull($this->localize->region('', true));
+
+        $this->assertEquals('Alberta', $this->localize->region('alberta'));
+        $this->assertEquals('British Columbia', $this->localize->region('british columbia'));
+        $this->assertEquals('Manitoba', $this->localize->region('manitoba'));
+        $this->assertEquals('New Brunswick', $this->localize->region('new brunswick'));
+        $this->assertEquals('Newfoundland and Labrador', $this->localize->region('newfoundland and labrador'));
+        $this->assertEquals('Northwest Territories', $this->localize->region('northwest territories'));
+        $this->assertEquals('Nova Scotia', $this->localize->region('nova scotia'));
+        $this->assertEquals('Nunavut', $this->localize->region('nunavut'));
+        $this->assertEquals('Ontario', $this->localize->region('ontario'));
+        $this->assertEquals('Prince Edward Island', $this->localize->region('prince edward island'));
+        $this->assertEquals('Quebec', $this->localize->region('quebec'));
+        $this->assertEquals('Saskatchewan', $this->localize->region('saskatchewan'));
+        $this->assertEquals('Yukon', $this->localize->region('yukon'));
+
+        $this->assertEquals('Alberta', $this->localize->region('ab'));
+        $this->assertEquals('British Columbia', $this->localize->region('bc'));
+        $this->assertEquals('Manitoba', $this->localize->region('mb'));
+        $this->assertEquals('New Brunswick', $this->localize->region('nb'));
+        $this->assertEquals('Newfoundland and Labrador', $this->localize->region('nl'));
+        $this->assertEquals('Northwest Territories', $this->localize->region('nt'));
+        $this->assertEquals('Nova Scotia', $this->localize->region('ns'));
+        $this->assertEquals('Nunavut', $this->localize->region('nu'));
+        $this->assertEquals('Ontario', $this->localize->region('on'));
+        $this->assertEquals('Prince Edward Island', $this->localize->region('pe'));
+        $this->assertEquals('Quebec', $this->localize->region('qc'));
+        $this->assertEquals('Saskatchewan', $this->localize->region('sk'));
+        $this->assertEquals('Yukon', $this->localize->region('yt'));
+    }
+
+    public function testPostalCode()
+    {
+        $match = 'V6B 3H7';
+        $this->assertEquals($match, $this->localize->postalCode('v6b3h7'));
+        $this->assertEquals($match, $this->localize->postalCode('v6b 3h7'));
+    }
+
+    public function testPostalCodeNull()
+    {
+        $this->assertNull($this->localize->postalCode('not a postal code'));
+        $this->assertNull($this->localize->postalCode('v66 3a7'));
+    }
+
+    public function testPhone()
+    {
+        $match = '123-456-7890';
+        $this->assertEquals($match, $this->localize->phone('1234567890'));
+        $this->assertEquals($match, $this->localize->phone('123 456 7890'));
+        $this->assertEquals($match, $this->localize->phone('123.456-7890'));
+        $this->assertEquals($match, $this->localize->phone('(123) 456-7890'));
+        $this->assertEquals($match, $this->localize->phone('+1 (123) 456-7890'));
+        $this->assertEquals($match, $this->localize->phone('1 1234567890'));
+    }
+
+    public function testPhoneNull()
+    {
+        $this->assertNull($this->localize->phone('not a phone number'));
+        $this->assertNull($this->localize->phone('abc-def-ghij'));
+    }
+}
---
+++ tests/LocalizeTest.php
@@ -0,0 +1,81 @@
+<?php namespace Localize\Tests;
+
+use Localize\Localize;
+use Localize\Exception\LocaleParseException;
+use Localize\Exception\LocaleSupportException;
+
+class LocalizeTest extends \PHPUnit_Framework_TestCase
+{
+    private $localize;
+
+    public function setup()
+    {
+        $this->localize = new Localize('CA');
+    }
+
+    public function testNewWithCountry()
+    {
+        $localize = new Localize('Canada');
+        $this->assertEquals('CA', $localize->getLocale());
+    }
+
+    public function testSetLocale()
+    {
+        $this->assertTrue($this->localize->setLocale('CA'));
+    }
+
+    public function testSetLocaleParseException()
+    {
+        try {
+            $this->localize->setLocale('BAD');
+        } catch (LocaleParseException $e) {
+            return;
+        }
+
+        $this->fail('An expected exception (LocaleParseException) has not been raised.');
+    }
+
+    public function testSetLocaleSupportException()
+    {
+        try {
+            $this->localize->setLocale('NONE');
+        } catch (LocaleSupportException $e) {
+            return;
+        }
+
+        $this->fail('An expected exception (LocaleSupportException) has not been raised.');
+    }
+
+    public function testGetLocale()
+    {
+        $this->assertEquals('CA', $this->localize->getLocale());
+    }
+
+    public function testRegex()
+    {
+        $result = $this->localize->regex('555-555-5555', '/^(\d{3})-(\d{3})-(\d{4})$/' , '$1 $2 $3');
+        $this->assertEquals('555 555 5555', $result);
+    }
+
+    public function testRegexWithOptionals()
+    {
+        $result = $this->localize->regex('123456789 AB 0123', '/^(\d{9})\D?([A-Z]{2})\D?(\d{4})$/' , '$1$2$3');
+        $this->assertEquals('123456789AB0123', $result);
+    }
+
+    public function testRegexManyToOne()
+    {
+        $regexes = array(
+            '/^(\d{3}) (\d{3}) (\d{4})$/',
+            '/^(\d{3})-(\d{3})-(\d{4})$/',
+        );
+
+        $replaces = '($1) $2-$3';
+
+        $result = $this->localize->regexManyToOne('555 555 5555', $regexes, $replaces);
+        $this->assertEquals('(555) 555-5555', $result);
+
+        $result = $this->localize->regexManyToOne('555-555-5555', $regexes, $replaces);
+        $this->assertEquals('(555) 555-5555', $result);
+    }
+}
